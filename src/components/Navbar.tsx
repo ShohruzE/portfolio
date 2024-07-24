fedc13975cf5b64eea7f9738dc8cf325b1e9c639
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-24 10:03:50 -0400

    Edit navbar to support other sections

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,16 +3,18 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className="py-8 bg-black text-white">
+    <div className="p-8 bg-black text-white">
         <div className="container mx-auto">
             <nav className="flex justify-between">
                 <div>
-                    <h1 className="font-bold text-2xl">Shohruz Ernazarov</h1>
+                    <h1 className="font-extrabold text-2xl">Shohruz.</h1>
                 </div>
                 <div>
                     <ul className="flex justify-center items-center gap-4">
                         <li><Link href="#about">About</Link></li>
+                        <li><Link href="#experience">Experience</Link></li>
                         <li><Link href="#projects">Projects</Link></li>
+                        <li><Link href="#skills">Skills</Link></li>
                     </ul>
                 </div>
                 <div>
