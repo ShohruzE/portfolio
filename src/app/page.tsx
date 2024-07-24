f575042a002c019320f249093e575d7745bd6b05
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-24 10:02:43 -0400

    Add other components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
 import Hero from "../components/Hero";
+import About from "../components/About";
+import Experience from "../components/Experience";
+import Projects from "../components/Projects";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="">
       <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
     </main>
   );
 }
