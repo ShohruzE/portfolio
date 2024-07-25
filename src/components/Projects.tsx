71aec3b9968e1626711fe69348549420515fb75b
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-25 17:19:45 -0400

    Remove project pictures and add responsiveness

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,29 +17,29 @@ const Projects = () => {
       name: 'HoneyDo',
       techStack: 'React.js, Node.js, Express.js',
       description: "For HackPrinceton Fall '23, created an AI-powered web-app for users to receive academic help on their homework, projects, and more.",
-      imageUrl: '/images/hunter-cs-club.jpg',
+      imageUrl: '/images/honeydo.jpg',
       githubUrl: 'https://github.com/ShohruzE/hackprinceton-f23',
     },
     {
       name: 'FRC 333 Robotics Website',
       techStack: 'Next.js, TailwindCSS, TypeScript',
-      description: 'The official website for the FRC 333 robotics team.',
-      imageUrl: '/images/hunter-cs-club.jpg',
+      description: 'The official website for the FRC 333 robotics team. Currently redesigning the website to be more user-friendly and accessible in Next.js and TailwindCSS.',
+      imageUrl: '/images/frc333.png',
       githubUrl: 'https://github.com/ShohruzE/jdhs-robotics-website/',
       demoUrl: 'https://www.frc333.com/'
     },
     {
       name: 'Metfindr',
       techStack: 'React.js',
-      description: 'This is a sample project using React and TailwindCSS.',
-      imageUrl: '/images/hunter-cs-club.jpg',
+      description: 'A front-end web application that allows users to find unique art pieces utilizing the MET API, without having to visist the MET Museum.',
+      imageUrl: '/images/metfindr.png',
       githubUrl: 'https://github.com/ShohruzE/metfindr',
       demoUrl: 'https://metfindr.netlify.app/'
     },
   ]
 
   return (
-    <section id="projects" className="my-20">
+    <section id="projects" className="my-20 mx-8">
       <div className="container mx-auto">
         <div className="flex flex-col justify-center items-center gap-8">
           <div>
@@ -47,23 +47,14 @@ const Projects = () => {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
             {projects.map((project, index) => (
-              <div key={index} className="flex flex-col md:flex-row gap-4 items-center border-2">
-                <div className="w-full md:w-1/2 border-r-2">
-                  <Image 
-                    src={project.imageUrl} 
-                    alt={project.name} 
-                    width={300} 
-                    height={200} 
-                    className="" 
-                  />
-                </div>
-                <div className="w-full md:w-1/2">
+              <div key={index} className="flex flex-col lg:flex-row gap-4 items-center border-2">
+                <div className="w-full p-4">
                   <h3 className="text-xl font-bold">{project.name}</h3>
                   <p className="text-sm text-gray-500">{project.techStack}</p>
-                  <p className="my-2">{project.description}</p>
+                  <p className="my-4">{project.description}</p>
                   <div className="flex justify-start items-center gap-4 font-bold">
-                    <Link href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="flex justify-center gap-1 bg-black text-white px-2 py-2 rounded-xl"><Github size={24}/>GitHub</Link>
-                    {project.demoUrl && <Link href={project.demoUrl} target="_blank" rel="noopener noreferrer">Demo</Link>}
+                    <Link href={project.githubUrl} target="_blank" rel="noopener noreferrer" className="flex justify-center gap-1 bg-black hover:bg-gray-500 text-white px-2 py-2 rounded-xl"><Github size={24}/>GitHub</Link>
+                    {project.demoUrl && <Link href={project.demoUrl} className="hover:text-gray-500" target="_blank" rel="noopener noreferrer">Demo</Link>}
                   </div>
                 </div>
               </div>
