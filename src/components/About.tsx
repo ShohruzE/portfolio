622fe68c7b7f7780c2ade26647bf4bfb500af4eb
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-24 23:33:55 -0400

    Fix spacing and tiny fix to description grammar

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 const About = () => {
   return (
-    <section id="about">
+    <section id="about" className="my-20">
         <div className="container mx-auto">
             <div className="flex justify-around items-center">
                 <div className="">
@@ -17,7 +17,7 @@ const About = () => {
                 </div>
                 <div className="w-1/2 flex flex-col gap-6">
                     <h2 className="text-3xl font-bold border-b-2 border-black rounded-m">About Me</h2>
-                    <p className="text-gray-500 text-l">I am a Junior studying Computer Science at Hunter College. I founded my own Computer Science Club in my sophomore year, leading to over 600 members accumulated in the span of an academic year. I'm a Software Engineering Fellow at Headstarter and CUNY Tech Prep, with aspirations to become a full-stack web developer. </p>
+                    <p className="text-gray-500 text-l">I am a Junior studying Computer Science at Hunter College. I founded my own Computer Science Club in my sophomore year, leading to over 600 members accumulated in the span of an academic year. I'm a Software Engineering Fellow at Headstarter and at CUNY Tech Prep, with aspirations to become a full-stack web developer with a focus in AI. </p>
                 </div>
             </div>
         </div>
