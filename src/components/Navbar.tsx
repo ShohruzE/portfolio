7cddfaecc5d445b8c35797e334a3cef0ca291f99
Shohruz Ernazarov <shohruz123456789@gmail.com>
2024-07-25 22:44:44 -0400

    Add hover states to links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,16 +10,16 @@ const Navbar = () => {
                     <h1 className="font-extrabold text-2xl">Shohruz.</h1>
                 </div>
                 <div>
-                    <ul className="hidden sm:flex justify-center items-center gap-4">
-                        <li><Link href="#about">About</Link></li>
-                        <li><Link href="#experience">Experience</Link></li>
-                        <li><Link href="#projects">Projects</Link></li>
-                        <li><Link href="#skills">Skills</Link></li>
+                    <ul className="hidden sm:flex justify-center items-center gap-4 text-l">
+                        <li><Link href="#about" className="pb-1 hover:border-b-2 hover:border-b-white">About</Link></li>
+                        <li><Link href="#experience" className="pb-1 hover:border-b-2 hover:border-b-white">Experience</Link></li>
+                        <li><Link href="#projects" className="pb-1 hover:border-b-2 hover:border-b-white">Projects</Link></li>
+                        <li><Link href="#skills" className="pb-1 hover:border-b-2 hover:border-b-white">Skills</Link></li>
                     </ul>
                 </div>
                 <div>
-                    <button>
-                        <Link href="#contact">Contact</Link>
+                    <button className="bg-white py-2 px-3 text-black font-bold rounded-xl hover:scale-105">
+                        <Link href="mailto:shohruzernazarov04@gmail.com">Contact</Link>
                     </button>
                 </div>
             </nav>
